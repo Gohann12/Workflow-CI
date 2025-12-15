7694db0bf018ff26a3579bfb0a07284649812e7e
Gohann12 <fauliarachman09@gmail.com>
2025-12-15 23:22:38 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -4,22 +4,26 @@ import mlflow.sklearn
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 
+
+# MLflow Project otomatis membuat run
 mlflow.set_experiment("clustering-experiment")
 
+# Load dataset (HARUS ADA DI FOLDER MLProject)
 data = pd.read_csv("bank_transactions_preprocessed.csv")
 data_numeric = data.select_dtypes(include=["int64", "float64"])
 
+# Training
 model = KMeans(n_clusters=3, random_state=42)
 labels = model.fit_predict(data_numeric)
 
+# Evaluasi
 sil_score = silhouette_score(data_numeric, labels)
 
+# Logging (INI BOLEH)
 mlflow.log_param("n_clusters", 3)
 mlflow.log_metric("silhouette_score", sil_score)
 
-mlflow.sklearn.log_model(
-    model,
-    artifact_path="model"
-)
+# Simpan model (INI WAJIB UNTUK NILAI)
+mlflow.sklearn.log_model(model, artifact_path="model")
 
-print("Training via MLflow Project selesai")
+print("MLflow Project training selesai")
