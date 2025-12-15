1378084996c8aef3ae679fced9ecdf81e13fb4d0
Gohann12 <fauliarachman09@gmail.com>
2025-12-15 23:11:00 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,22 +6,20 @@ from sklearn.metrics import silhouette_score
 
 mlflow.set_experiment("clustering-experiment")
 
-with mlflow.start_run(run_name="ci-training"):
+data = pd.read_csv("bank_transactions_preprocessed.csv")
+data_numeric = data.select_dtypes(include=["int64", "float64"])
 
-    data = pd.read_csv("bank_transactions_preprocessed.csv")
-    data_numeric = data.select_dtypes(include=["int64", "float64"])
+model = KMeans(n_clusters=3, random_state=42)
+labels = model.fit_predict(data_numeric)
 
-    model = KMeans(n_clusters=3, random_state=42)
-    labels = model.fit_predict(data_numeric)
+sil_score = silhouette_score(data_numeric, labels)
 
-    sil_score = silhouette_score(data_numeric, labels)
+mlflow.log_param("n_clusters", 3)
+mlflow.log_metric("silhouette_score", sil_score)
 
-    mlflow.log_param("n_clusters", 3)
-    mlflow.log_metric("silhouette_score", sil_score)
+mlflow.sklearn.log_model(
+    model,
+    artifact_path="model"
+)
 
-    mlflow.sklearn.log_model(
-        model,
-        artifact_path="model"
-    )
-
-    print("Training & logging selesai")
+print("Training via MLflow Project selesai")
